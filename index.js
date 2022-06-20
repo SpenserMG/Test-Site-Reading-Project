3c3b38682c7c8bfcef62d9a241c8c9639462d3a2
SpenserMG <spensermgreeley@gmail.com>
2022-06-20 16:12:42 -0400

    Fetch request connected to live server in azure

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function boldtext() {
     document.getElementById("displayContainer").innerHTML = display;
 }
 
-// This was the original hardcoded fetch request that I was trying before
+// This was the original hardcoded fetch request that I was trying when first grappling with fetch
+
 // window.addEventListener("load", () => {
 //     //CIRRO.init()
 //     fetch('http://localhost:8080/hello?message=' +, {
@@ -19,18 +20,17 @@ function boldtext() {
 async function tryingFetch() {
     const inputChanged = await doFetch();
     document.getElementById("displayContainer").innerHTML = inputChanged;
+    document.getElementById("entry").value = "";
 }
 
 async function doFetch() {
     //var inputChanged = ""
     const entryInput = document.getElementById("entry");
     const inputValue = entryInput.value;
-    const fetchurl = `http://localhost:8080/hello?message=${inputValue}`
+    const fetchurl = `https://readgood.azurewebsites.net/hello?message=${inputValue}`
     return fetch(fetchurl)
         .then(response => response.json())
         .then((response) => response.textBlock)
         //.then(json => { document.getElementById("displayContainer").innerHTML = json })
         .catch(err => console.log(err))
 }
-
-//document.getElementById("displayContainer").innerHTML = inputChanged;
