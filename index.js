8e2b9035542534083d92165530971ce9c0b22c4c
SpenserMG <spensermgreeley@gmail.com>
2022-06-20 12:27:38 -0400

    JSON text is being displayed properly

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ function boldtext() {
     document.getElementById("displayContainer").innerHTML = display;
 }
 
-
-
-// This is just a namespace 
-
-//window.onload = CIRRO.init;
-
+// This was the original hardcoded fetch request that I was trying before
 // window.addEventListener("load", () => {
 //     //CIRRO.init()
 //     fetch('http://localhost:8080/hello?message=' +, {
@@ -26,15 +21,14 @@ async function tryingFetch() {
     document.getElementById("displayContainer").innerHTML = inputChanged;
 }
 
-
 async function doFetch() {
     //var inputChanged = ""
     const entryInput = document.getElementById("entry");
     const inputValue = entryInput.value;
     const fetchurl = `http://localhost:8080/hello?message=${inputValue}`
-    fetch(fetchurl)
-        .then(response => { return response.json() })
-        .then((response) => { console.log(response) })
+    return fetch(fetchurl)
+        .then(response => response.json())
+        .then((response) => response.textBlock)
         //.then(json => { document.getElementById("displayContainer").innerHTML = json })
         .catch(err => console.log(err))
 }
